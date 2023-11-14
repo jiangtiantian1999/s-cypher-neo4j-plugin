de98e012f4845d94e2e6f4dd7ec6aa719835c774
Jiangtiantian1999 <jiangtiantian1999@163.com>
2023-11-14 23:42:18 +0800

    add getValueEffectiveTime

--- src/main/java/cn/scypher/neo4j/plugin/ReadingQuery.java
+++ src/main/java/cn/scypher/neo4j/plugin/ReadingQuery.java
@@ -25,59 +25,77 @@ public class ReadingQuery {
         return null;
     }
 
+    public List<Node> getValueNodes(Node propertyNode, @Name("timeWindow") Object timeWindow) {
+        ResourceIterable<Relationship> relationships = propertyNode.getRelationships(Direction.OUTGOING, RelationshipType.withName("PROPERTY_VALUE"));
+        List<Node> valueNodeList = new ArrayList<>();
+        // snapshot/scope语句指定的时间区间
+        STimePoint snapshotTimePoint = GlobalVariablesManager.getSnapshotTimePoint();
+        SInterval scopeInterval = GlobalVariablesManager.getScopeInterval();
+        STimePoint valueNodeTimePoint = null;
+        SInterval valueNodeInterval = null;
+        if (timeWindow != null) {
+            if (timeWindow instanceof LocalDate | timeWindow instanceof OffsetTime | timeWindow instanceof LocalTime | timeWindow instanceof ZonedDateTime | timeWindow instanceof LocalDateTime) {
+                valueNodeTimePoint = new STimePoint(timeWindow);
+            } else if (timeWindow instanceof Map) {
+                valueNodeInterval = new SInterval((Map<String, Object>) timeWindow);
+            } else {
+                throw new RuntimeException("Type mismatch: expected Date, Time, LocalTime, LocalDateTime, DateTime or Interval but was " + timeWindow.getClass().getSimpleName());
+            }
+        }
+        for (Relationship relationship : relationships) {
+            Node valueNode = relationship.getEndNode();
+            SInterval valueNodeEffectiveTime = new SInterval(new STimePoint(valueNode.getProperty("intervalFrom")), new STimePoint(valueNode.getProperty("intervalTo")));
+            if (valueNodeTimePoint == null && valueNodeInterval == null && snapshotTimePoint == null && scopeInterval == null) {
+                valueNodeList.add(valueNode);
+            } else if (valueNodeTimePoint != null && valueNodeEffectiveTime.contains(valueNodeTimePoint)) {
+                valueNodeList.add(valueNode);
+            } else if (valueNodeInterval != null && valueNodeEffectiveTime.contains(valueNodeInterval)) {
+                valueNodeList.add(valueNode);
+            } else if (scopeInterval != null && valueNodeEffectiveTime.contains(scopeInterval)) {
+                valueNodeList.add(valueNode);
+            } else if (snapshotTimePoint != null && valueNodeEffectiveTime.contains(snapshotTimePoint)) {
+                valueNodeList.add(valueNode);
+            }
+        }
+        return valueNodeList;
+    }
+
 
     /**
-     * @param objectNode   对象节点
-     * @param propertyName 对象节点的属性
+     * @param object       对象节点/边/Map
+     * @param propertyName 对象节点/边/Map的属性
      * @param timeWindow   值节点的有效时间限制
-     * @return 返回对象节点的某个属性的属性值，如果在限制时间窗口内有多个属性值，返回一个列表；如果在限制时间窗口内有一个属性值，返回该值；如果在限制时间窗口内没有属性值，返回null。
+     * @return 在返回对象节点的某个属性的属性值时，如果在限制时间窗口内有多个属性值，返回一个列表；如果在限制时间窗口内有一个属性值，返回该值；如果在限制时间窗口内没有属性值，返回null。
      */
     @UserFunction("scypher.getPropertyValue")
     @Description("Get the property value of object node.")
-    public Object getPropertyValue(@Name("node") Node objectNode, @Name("propertyName") String propertyName, @Name("timeWindow") Object timeWindow) {
-        if (objectNode != null && propertyName != null) {
-            Node propertyNode = getPropertyNode(objectNode, propertyName);
-            if (propertyNode != null) {
-                ResourceIterable<Relationship> relationships = propertyNode.getRelationships(Direction.OUTGOING, RelationshipType.withName("PROPERTY_VALUE"));
-                List<Object> propertyValueList = new ArrayList<>();
-                // snapshot/scope语句指定的时间区间
-                STimePoint snapshotTimePoint = GlobalVariablesManager.getSnapshotTimePoint();
-                SInterval scopeInterval = GlobalVariablesManager.getScopeInterval();
-                STimePoint valueNodeTimePoint = null;
-                SInterval valueNodeInterval = null;
-                if (timeWindow != null) {
-                    if (timeWindow instanceof LocalDate | timeWindow instanceof OffsetTime | timeWindow instanceof LocalTime | timeWindow instanceof ZonedDateTime | timeWindow instanceof LocalDateTime) {
-                        valueNodeTimePoint = new STimePoint(timeWindow);
-                    } else if (timeWindow instanceof Map) {
-                        valueNodeInterval = new SInterval((Map<String, Object>) timeWindow);
-                    } else {
-                        throw new RuntimeException("Type mismatch: expected Date, Time, LocalTime, LocalDateTime, DateTime or Interval but was " + timeWindow.getClass().getSimpleName());
-                    }
-                }
-                for (Relationship relationship : relationships) {
-                    Node valueNode = relationship.getEndNode();
-                    SInterval valueNodeEffectiveTime = new SInterval(new STimePoint(valueNode.getProperty("intervalFrom")), new STimePoint(valueNode.getProperty("intervalTo")));
-                    if (valueNodeTimePoint == null && valueNodeInterval == null && snapshotTimePoint == null && scopeInterval == null) {
-                        propertyValueList.add(valueNode.getProperty("content"));
-                    } else if (valueNodeTimePoint != null && valueNodeEffectiveTime.contains(valueNodeTimePoint)) {
-                        propertyValueList.add(valueNode.getProperty("content"));
-                    } else if (valueNodeInterval != null && valueNodeEffectiveTime.contains(valueNodeInterval)) {
-                        propertyValueList.add(valueNode.getProperty("content"));
-                    } else if (scopeInterval != null && valueNodeEffectiveTime.contains(scopeInterval)) {
-                        propertyValueList.add(valueNode.getProperty("content"));
-                    } else if (snapshotTimePoint != null && valueNodeEffectiveTime.contains(snapshotTimePoint)) {
+    public Object getPropertyValue(@Name("object") Object object, @Name("propertyName") String propertyName, @Name("timeWindow") Object timeWindow) {
+        if (object != null && propertyName != null) {
+            if (object instanceof Node objectNode) {
+                Node propertyNode = getPropertyNode(objectNode, propertyName);
+                if (propertyNode != null) {
+                    List<Node> valueNodeList = getValueNodes(propertyNode, timeWindow);
+                    List<Object> propertyValueList = new ArrayList<>();
+                    for (Node valueNode : valueNodeList) {
                         propertyValueList.add(valueNode.getProperty("content"));
                     }
-                }
-                if (propertyValueList.size() == 0) {
+                    if (propertyValueList.size() == 0) {
+                        return null;
+                    }
+                    if (propertyValueList.size() == 1) {
+                        return propertyValueList.get(0);
+                    }
+                    return propertyValueList;
+                } else {
                     return null;
                 }
-                if (propertyValueList.size() == 1) {
-                    return propertyValueList.get(0);
-                }
-                return propertyValueList;
+            } else if (object instanceof Relationship relationship) {
+                return relationship.getProperty(propertyName);
+            } else if (object instanceof Map) {
+                Map<String, Object> objectMap = (Map<String, Object>) object;
+                return objectMap.getOrDefault(propertyName, null);
             } else {
-                return null;
+                throw new RuntimeException("Type mismatch: expected Map, Node, Relationship, Point, Duration, Date, Time, LocalTime, LocalDateTime or DateTime but was " + object.getClass().getSimpleName());
             }
         } else {
             throw new RuntimeException("Missing parameter");
@@ -112,6 +130,38 @@ public class ReadingQuery {
                 return null;
             } else {
                 throw new RuntimeException("Type mismatch: expected Node or Map but was " + objectNode.getClass().getSimpleName());
+            }
+        } else {
+            throw new RuntimeException("Missing parameter");
+        }
+    }
+
+    /**
+     * @param objectNode   对象节点
+     * @param propertyName 属性名
+     * @param timeWindow   值节点的有效时间限制
+     * @return 返回对象节点的某个属性下的满足有效时间限制的所有值节点
+     */
+    @UserFunction("scypher.getValueEffectiveTime")
+    @Description("Get the effective time of value node.")
+    public Object getValueEffectiveTime(@Name("node") Node objectNode, @Name("propertyName") String propertyName, @Name("timeWindow") Object timeWindow) {
+        if (objectNode != null && propertyName != null) {
+            Node propertyNode = getPropertyNode(objectNode, propertyName);
+            if (propertyNode != null) {
+                List<Node> valueNodeList = getValueNodes(propertyNode, timeWindow);
+                List<Map<String, Object>> valueEffectiveTimeList = new ArrayList<>();
+                for (Node valueNode : valueNodeList) {
+                    valueEffectiveTimeList.add(new SInterval(new STimePoint(valueNode.getProperty("intervalFrom")), new STimePoint(valueNode.getProperty("intervalTo"))).getSystemInterval());
+                }
+                if (valueEffectiveTimeList.size() == 0) {
+                    return null;
+                }
+                if (valueEffectiveTimeList.size() == 1) {
+                    return valueEffectiveTimeList.get(0);
+                }
+                return valueEffectiveTimeList;
+            } else {
+                return null;
             }
         } else {
             throw new RuntimeException("Missing parameter");
--- src/test/java/cn/scypher/neo4j/plugin/ReadingQueryTest.java
+++ src/test/java/cn/scypher/neo4j/plugin/ReadingQueryTest.java
@@ -68,4 +68,23 @@ public class ReadingQueryTest {
             System.out.println(record);
         }
     }
+
+    @Test
+    public void testGetValueEffectiveTime() {
+        System.out.println("testGetValueEffectiveTime");
+        this.session.run("CREATE (n:Person:Object {intervalFrom:scypher.timePoint('2010'), intervalTo:scypher.timePoint('NOW')})-[:OBJECT_PROPERTY]->" +
+                "(p:Property {content:'name', intervalFrom:scypher.timePoint('2010'), intervalTo:scypher.timePoint('NOW')})-[:PROPERTY_VALUE]->" +
+                "(v1:Value {content:'Nick', intervalFrom:scypher.timePoint('2010'), intervalTo:scypher.timePoint('2022')})," +
+                "(p)-[:PROPERTY_VALUE]->(v2:Value {content:'Tom', intervalFrom:scypher.timePoint('2023'), intervalTo:scypher.timePoint('NOW')})");
+        List<Record> records = this.session.run("MATCH (n:Person)" +
+                "RETURN scypher.getValueEffectiveTime(n,'name',NULL)").list();
+        for (Record record : records) {
+            System.out.println(record);
+        }
+        records = this.session.run("MATCH (n:Person)" +
+                "RETURN scypher.getValueEffectiveTime(n,'name',scypher.interval('2010','2022'))").list();
+        for (Record record : records) {
+            System.out.println(record);
+        }
+    }
 }
